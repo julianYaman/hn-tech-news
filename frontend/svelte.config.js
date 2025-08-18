c8c9d632968c17458db46aff96a1cafd8f2c7107
Julian <mail@yaman.pro>
2025-08-19 00:13:55 +0200

    fix: wrong svelte adapter used

--- frontend/svelte.config.js
+++ frontend/svelte.config.js
@@ -1,7 +1,9 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-node';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 export default {
   kit: {
     adapter: adapter()
-  }
+  },
+  preprocess: vitePreprocess()
 };
